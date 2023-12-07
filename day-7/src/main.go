0d161ab62b69101f847e84d027e8c7f86c6c2af3
bandreit <andy27hush@gmail.com>
2023-12-07 19:03:56 +0100

    probably the worst code I have written in my life

--- day-7/src/main.go
+++ day-7/src/main.go
@@ -23,7 +23,7 @@ func cardToValue(card string) int {
 	case "Q":
 		return 12
 	case "J":
-		return 11
+		return 1
 	case "T":
 		return 10
 	default:
@@ -47,7 +47,7 @@ func main() {
 	dat, err := os.ReadFile("../input.txt")
 	check(err)
 
-	fmt.Println(part1(dat))
+	fmt.Println("Part 2:", part1(dat))
 }
 
 func part1(dat []byte) int {
@@ -63,29 +63,55 @@ func part1(dat []byte) int {
 
 		cards := strings.Split(hand, "")
 
-		mapOfOccurrences := make(map[string]int)
+		mapOfOccurrencesByCard := make(map[string]int)
 		occurrencesArray := make([]int, 0)
 
 		for _, card := range cards {
 			occurrences := strings.Count(hand, string(card))
-			mapOfOccurrences[string(card)] = occurrences
+			mapOfOccurrencesByCard[string(card)] = occurrences
 		}
 
-		for _, value := range mapOfOccurrences {
+		for _, value := range mapOfOccurrencesByCard {
 			occurrencesArray = append(occurrencesArray, value)
 		}
 
 		sort.Ints(occurrencesArray)
+		// ok till here
+
+		if strings.Contains(hand, "J") {
+			numberOfJacks := strings.Count(hand, "J")
+
+			arrLen := len(occurrencesArray)
+			if arrLen > 1 {
+				if (occurrencesArray[arrLen-1]) == numberOfJacks {
+					occurrencesArray[arrLen-1] = occurrencesArray[arrLen-1] + occurrencesArray[arrLen-2]
+					occurrencesArray[arrLen-2] = 0
+				} else if occurrencesArray[arrLen-1] == occurrencesArray[arrLen-2] {
+					if numberOfJacks == 1 {
+						occurrencesArray = []int{2, 3}
+					}
+				} else {
+					occurrencesArray[arrLen-1] = occurrencesArray[arrLen-1] + numberOfJacks
+					occurrencesArray[arrLen-2] = occurrencesArray[arrLen-2] - numberOfJacks
+				}
+			}
+
+		}
+
+		for i := 0; i < len(occurrencesArray); i++ {
+			if occurrencesArray[i] == 0 {
+				occurrencesArray = append(occurrencesArray[:i], occurrencesArray[i+1:]...)
+			}
+		}
+
 		combo := make([]string, 0)
 
 		for _, value := range occurrencesArray {
 			str := strconv.Itoa(value)
 			combo = append(combo, str)
 		}
-
-		pokerHand := strings.Join(combo, "")
 		mapOfBidsByHand[hand] = bid
-
+		pokerHand := strings.Join(combo, "")
 		power := mapOfPowers[pokerHand]
 		if mapOfCardsByPower[power] == nil {
 			mapOfCardsByPower[power] = make([]string, 0)
@@ -107,8 +133,6 @@ func part1(dat []byte) int {
 	for _, v := range mapOfCardsByPower {
 		rank += len(v)
 	}
-
-	fmt.Println("top rank", rank)
 
 	for _, k := range keys {
 		nrOfCombinations := len(mapOfCardsByPower[k])
@@ -139,3 +163,26 @@ func part1(dat []byte) int {
 
 	return totalWinningHands
 }
+
+// 2345A 1
+// J345A 2
+// 2345J 3
+// 32T3K 4 - one pair
+// KK677 5 - two pairs
+// T3Q33 6 - three of a kind
+// Q2KJJ 7 - three of a kind
+
+// T3T3J = T3T3T 10 - full house - should be here
+// Q2Q2Q 8 - full house
+// 2AAAA 9 - four of a kind
+// T3T3J 10 - full house
+
+// T55J5 11 - four of a kind
+// QQQJA 12 - four of a kind
+// KTJJT 13 - four of a kind
+// JJJJJ 14
+// JJJJ2 15
+// JAAAA 16
+// 2JJJJ 17
+// AAAAJ 18
+// AAAAA 19
