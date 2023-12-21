171a3fe6b7916023bc61d9ea8d200c8cea398f88
bandreit <andy27hush@gmail.com>
2023-12-21 15:49:17 +0200

    day 17 done

--- day-17/src/main.go
+++ day-17/src/main.go
@@ -1,144 +1,61 @@
 package main
 
 import (
-	"container/heap"
 	"fmt"
 	"os"
-	"slices"
 	"strconv"
 	"strings"
 )
 
-type PriorityQueue []*Node
-
-func (pq PriorityQueue) Len() int { return len(pq) }
-
-func (pq PriorityQueue) Less(i, j int) bool {
-	return pq[i].Distance < pq[j].Distance
+type HeatState struct {
+	point, dir Point
+	streak     int
 }
 
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].Index = i
-	pq[j].Index = j
+type Point struct {
+	x int
+	y int
 }
 
-func (pq *PriorityQueue) Push(x interface{}) {
-	n := len(*pq)
-	node := x.(*Node)
-	node.Index = n
-	*pq = append(*pq, node)
-}
+func Dijkstra(grid map[Point]int, start, target Point) int {
+	dir1 := Point{start.x, start.y + 1}
+	dir2 := Point{start.x + 1, start.y}
+	pointsToCheck := []HeatState{{start, dir1, 0}, {start, dir2, 0}}
+	dist := map[HeatState]int{{start, Point{0, 0}, 0}: 0}
+	minHeatLoss := int(^uint(0) >> 1)
 
-func (pq *PriorityQueue) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	node := old[n-1]
-	node.Index = -1
-	*pq = old[0 : n-1]
-	return node
-}
+	for len(pointsToCheck) > 0 {
+		node := pointsToCheck[0]
+		pointsToCheck = pointsToCheck[1:]
 
-type Position struct {
-	X, Y int
-}
-
-type Node struct {
-	Pos       Position
-	Direction Position
-	Distance  int
-	Straight  int
-	Index     int
-}
-
-func Dijkstra(grid map[Position]int, start, target Position) []Position {
-	dist := make(map[Position]int)
-	prev := make(map[Position]Position)
-
-	for pos := range grid {
-		dist[pos] = int(^uint(0) >> 1) // Max int value
-	}
-
-	dist[start] = 0
-
-	pq := make(PriorityQueue, 0)
-	heap.Init(&pq)
-	dir1 := Position{start.X, start.Y + 1}
-	dir2 := Position{start.X + 1, start.Y}
-
-	prev[dir1] = start
-	prev[dir2] = start
-
-	heap.Push(&pq, &Node{Pos: start, Direction: dir1, Distance: 0, Straight: 0})
-	heap.Push(&pq, &Node{Pos: start, Direction: dir2, Distance: 0, Straight: 0})
-
-	for pq.Len() > 0 {
-		node := heap.Pop(&pq).(*Node)
-
-		if node.Pos == target {
-			break
+		if node.point == target && node.streak >= 4 {
+			minHeatLoss = min(minHeatLoss, dist[node])
 		}
 
-		for _, dir := range [3]Position{node.Direction, dirLeft(node.Direction), dirRight(node.Direction)} {
-			// skip if dir is out of bounds
-			nextPoint := Position{node.Pos.X + dir.X, node.Pos.Y + dir.Y}
+		for _, dir := range [3]Point{node.dir, dirLeft(node.dir), dirRight(node.dir)} {
+			nextPoint := Point{node.point.x + dir.x, node.point.y + dir.y}
 			if _, ok := grid[nextPoint]; !ok {
 				continue
 			}
 
-			alt := dist[node.Pos] + grid[nextPoint]
-
+			alt := dist[node] + grid[nextPoint]
 			newStraight := 1
-			if node.Direction == dir {
-				newStraight = node.Straight + 1
+			if node.dir == dir {
+				newStraight = node.streak + 1
 			}
 
-			if ((node.Direction == dir) && node.Straight < 3) ||
-				(dir != node.Direction) {
-				if alt < dist[nextPoint] {
-					dist[nextPoint] = alt
-					prev[nextPoint] = node.Pos
-
-					heap.Push(&pq, &Node{Pos: nextPoint, Direction: dir, Distance: alt, Straight: newStraight})
+			if ((node.dir == dir) && node.streak < 10) ||
+				(dir != node.dir && node.streak >= 4) {
+				nextState := HeatState{point: nextPoint, dir: dir, streak: newStraight}
+				if val, found := dist[nextState]; !found || val > alt {
+					dist[nextState] = alt
+					pointsToCheck = append(pointsToCheck, nextState)
 				}
 			}
 		}
 	}
 
-	path := make([]Position, 0)
-	curr := target
-	for curr != start {
-		path = append(path, curr)
-		curr = prev[curr]
-	}
-	path = append(path, start)
-	reverse(path)
-
-	return path
-}
-
-func getAvailableDirections(pos Position, prev map[Position]Position) []Position {
-	neighbors := []Position{
-		{pos.X - 1, pos.Y},
-		{pos.X + 1, pos.Y},
-		{pos.X, pos.Y - 1},
-		{pos.X, pos.Y + 1},
-	}
-
-	prevNode, ok := prev[pos]
-	if ok {
-		neighbors = slices.DeleteFunc(neighbors, func(neighbor Position) bool {
-			return neighbor == prevNode
-		})
-	}
-
-	return neighbors
-}
-
-func reverse(path []Position) {
-	for i, j := 0, len(path)-1; i < j; i, j = i+1, j-1 {
-		path[i], path[j] = path[j], path[i]
-	}
+	return minHeatLoss
 }
 
 func check(e error) {
@@ -154,43 +71,28 @@ func main() {
 	txt := strings.ReplaceAll(string(dat), "\n\n", "")
 	lines := strings.Split(txt, "\n")
 
-	grid := map[Position]int{}
-	start := Position{0, 0}
-	target := Position{0, 0}
+	grid := map[Point]int{}
+	start := Point{0, 0}
+	target := Point{0, 0}
 
 	for i, line := range lines {
 		for j, char := range line {
 			number, err := strconv.Atoi(string(char))
 			check(err)
 
-			grid[Position{i, j}] = number
-			target = Position{i, j}
+			grid[Point{i, j}] = number
+			target = Point{i, j}
 		}
 	}
 
-	path := Dijkstra(grid, start, target)
-
-	fmt.Println("Shortest path:")
-	for _, pos := range path {
-		fmt.Println(pos)
-	}
-
-	for _, pos := range path {
-		grid[pos] = 0
-	}
-
-	for i, line := range lines {
-		for j := range line {
-			fmt.Printf("%d", grid[Position{i, j}])
-		}
-		fmt.Println()
-	}
+	minPath := Dijkstra(grid, start, target)
+	fmt.Println(minPath)
 }
 
-func dirLeft(p Position) Position {
-	return Position{p.Y, -p.X}
+func dirLeft(p Point) Point {
+	return Point{p.y, -p.x}
 }
 
-func dirRight(p Position) Position {
-	return Position{-p.Y, p.X}
+func dirRight(p Point) Point {
+	return Point{-p.y, p.x}
 }
