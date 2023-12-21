f586f35257944cac26b0929e206a159ef09bc03d
bandreit <andy27hush@gmail.com>
2023-12-21 12:58:28 +0200

    retrying

--- day-17/src/main.go
+++ day-17/src/main.go
@@ -1,12 +1,145 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
-	"math"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
+
+type PriorityQueue []*Node
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].Distance < pq[j].Distance
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].Index = i
+	pq[j].Index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	node := x.(*Node)
+	node.Index = n
+	*pq = append(*pq, node)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	node := old[n-1]
+	node.Index = -1
+	*pq = old[0 : n-1]
+	return node
+}
+
+type Position struct {
+	X, Y int
+}
+
+type Node struct {
+	Pos       Position
+	Direction Position
+	Distance  int
+	Straight  int
+	Index     int
+}
+
+func Dijkstra(grid map[Position]int, start, target Position) []Position {
+	dist := make(map[Position]int)
+	prev := make(map[Position]Position)
+
+	for pos := range grid {
+		dist[pos] = int(^uint(0) >> 1) // Max int value
+	}
+
+	dist[start] = 0
+
+	pq := make(PriorityQueue, 0)
+	heap.Init(&pq)
+	dir1 := Position{start.X, start.Y + 1}
+	dir2 := Position{start.X + 1, start.Y}
+
+	prev[dir1] = start
+	prev[dir2] = start
+
+	heap.Push(&pq, &Node{Pos: start, Direction: dir1, Distance: 0, Straight: 0})
+	heap.Push(&pq, &Node{Pos: start, Direction: dir2, Distance: 0, Straight: 0})
+
+	for pq.Len() > 0 {
+		node := heap.Pop(&pq).(*Node)
+
+		if node.Pos == target {
+			break
+		}
+
+		for _, dir := range [3]Position{node.Direction, dirLeft(node.Direction), dirRight(node.Direction)} {
+			// skip if dir is out of bounds
+			nextPoint := Position{node.Pos.X + dir.X, node.Pos.Y + dir.Y}
+			if _, ok := grid[nextPoint]; !ok {
+				continue
+			}
+
+			alt := dist[node.Pos] + grid[nextPoint]
+
+			newStraight := 1
+			if node.Direction == dir {
+				newStraight = node.Straight + 1
+			}
+
+			if ((node.Direction == dir) && node.Straight < 3) ||
+				(dir != node.Direction) {
+				if alt < dist[nextPoint] {
+					dist[nextPoint] = alt
+					prev[nextPoint] = node.Pos
+
+					heap.Push(&pq, &Node{Pos: nextPoint, Direction: dir, Distance: alt, Straight: newStraight})
+				}
+			}
+		}
+	}
+
+	path := make([]Position, 0)
+	curr := target
+	for curr != start {
+		path = append(path, curr)
+		curr = prev[curr]
+	}
+	path = append(path, start)
+	reverse(path)
+
+	return path
+}
+
+func getAvailableDirections(pos Position, prev map[Position]Position) []Position {
+	neighbors := []Position{
+		{pos.X - 1, pos.Y},
+		{pos.X + 1, pos.Y},
+		{pos.X, pos.Y - 1},
+		{pos.X, pos.Y + 1},
+	}
+
+	prevNode, ok := prev[pos]
+	if ok {
+		neighbors = slices.DeleteFunc(neighbors, func(neighbor Position) bool {
+			return neighbor == prevNode
+		})
+	}
+
+	return neighbors
+}
+
+func reverse(path []Position) {
+	for i, j := 0, len(path)-1; i < j; i, j = i+1, j-1 {
+		path[i], path[j] = path[j], path[i]
+	}
+}
 
 func check(e error) {
 	if e != nil {
@@ -14,150 +147,50 @@ func check(e error) {
 	}
 }
 
-type Point struct {
-	X, Y int
-}
-
 func main() {
 	dat, err := os.ReadFile("../input.txt")
 	check(err)
 
-	distances := [][]int{}
-
 	txt := strings.ReplaceAll(string(dat), "\n\n", "")
-	input := strings.Split(txt, "\n")
+	lines := strings.Split(txt, "\n")
 
-	for _, line := range input {
-		row := []int{}
-		for _, char := range line {
+	grid := map[Position]int{}
+	start := Position{0, 0}
+	target := Position{0, 0}
+
+	for i, line := range lines {
+		for j, char := range line {
 			number, err := strconv.Atoi(string(char))
 			check(err)
 
-			row = append(row, number)
+			grid[Position{i, j}] = number
+			target = Position{i, j}
 		}
-		distances = append(distances, row)
 	}
 
-	rows := len(distances)
-	cols := len(distances[0])
+	path := Dijkstra(grid, start, target)
 
-	start := Point{0, 0}
-	goal := Point{rows - 1, cols - 1}
-
-	shortestPath := AStar(start, goal, distances, ManhattanDistance)
-	fmt.Println(shortestPath)
-
-	for _, point := range shortestPath {
-		distances[point.X][point.Y] = -1
+	fmt.Println("Shortest path:")
+	for _, pos := range path {
+		fmt.Println(pos)
 	}
 
-	for _, row := range distances {
-		fmt.Println(row)
+	for _, pos := range path {
+		grid[pos] = 0
+	}
+
+	for i, line := range lines {
+		for j := range line {
+			fmt.Printf("%d", grid[Position{i, j}])
+		}
+		fmt.Println()
 	}
 }
 
-func reconstructPath(cameFrom map[Point]Point, current Point) []Point {
-	totalPath := []Point{current}
-	for current != (Point{}) {
-		current = cameFrom[current]
-		totalPath = append([]Point{current}, totalPath...)
-	}
-	return totalPath
+func dirLeft(p Position) Position {
+	return Position{p.Y, -p.X}
 }
 
-func AStar(start Point, goal Point, distances [][]int, h func(Point, Point) float64) []Point {
-	rows := len(distances)
-	cols := len(distances[0])
-
-	openSet := map[Point]bool{start: true}
-	cameFrom := make(map[Point]Point)
-	gScore := make(map[Point]float64)
-	for i := 0; i < rows; i++ {
-		for j := 0; j < cols; j++ {
-			gScore[Point{i, j}] = math.Inf(1)
-		}
-	}
-	gScore[start] = 0
-	fScore := make(map[Point]float64)
-	for i := 0; i < rows; i++ {
-		for j := 0; j < cols; j++ {
-			fScore[Point{i, j}] = math.Inf(1)
-		}
-	}
-	fScore[start] = h(start, goal)
-
-	for len(openSet) > 0 {
-		var current Point
-		minFScore := math.Inf(1)
-		for node := range openSet {
-			if fScore[node] < minFScore {
-				minFScore = fScore[node]
-				current = node
-			}
-		}
-
-		if current == goal {
-			return reconstructPath(cameFrom, current)
-		}
-
-		delete(openSet, current)
-
-		neighbors := []Point{
-			{current.X - 1, current.Y}, // left
-			{current.X, current.Y - 1}, // up
-			{current.X + 1, current.Y}, // right
-			{current.X, current.Y + 1}, // down
-		}
-
-		for _, neighbor := range neighbors {
-			// Skip if neighbor is out of bounds
-			if neighbor.X < 0 || neighbor.X >= rows || neighbor.Y < 0 || neighbor.Y >= cols {
-				continue
-			}
-
-			lastCameFroms := reconstructPath(cameFrom, current)
-			last3CameFroms := make([]Point, 0)
-			for i := len(lastCameFroms) - 1; i >= 0; i-- {
-				last3CameFroms = append(last3CameFroms, lastCameFroms[i])
-				if len(last3CameFroms) == 3 {
-					break
-				}
-			}
-
-			if len(last3CameFroms) == 3 {
-				initialPosition := last3CameFroms[0]
-				areOnTheSameAxis := areOnTheSameAxis(last3CameFroms, initialPosition, neighbor)
-
-				if areOnTheSameAxis {
-					if initialPosition.X == neighbor.X || initialPosition.Y == neighbor.Y {
-						continue
-					}
-				}
-			}
-
-			tentativeGScore := gScore[current] + float64(distances[neighbor.X][neighbor.Y])
-			if tentativeGScore < gScore[neighbor] {
-				cameFrom[neighbor] = current
-				gScore[neighbor] = tentativeGScore
-				fScore[neighbor] = tentativeGScore + h(neighbor, goal)
-				openSet[neighbor] = true
-			}
-		}
-	}
-
-	return nil
-}
-
-func areOnTheSameAxis(last3CameFroms []Point, initialPosition Point, neighbor Point) bool {
-	for i := 0; i < 2; i++ {
-		if last3CameFroms[i].X != last3CameFroms[i+1].X && last3CameFroms[i].Y != last3CameFroms[i+1].Y {
-			return false
-		}
-	}
-
-	return true
-}
-
-func ManhattanDistance(p1 Point, p2 Point) float64 {
-	return math.Abs(float64(p1.X-p2.X)) + math.Abs(float64(p1.Y-p2.Y))
+func dirRight(p Position) Position {
+	return Position{-p.Y, p.X}
 }
